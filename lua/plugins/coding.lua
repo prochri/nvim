cb4a75a7518b8e2fbbe5af06705e622155ebde66
Christoph Probst <38609485+prochri@users.noreply.github.com>
2024-05-18 08:37:54 +0200

    remove lua-json5

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -4,19 +4,6 @@ return {
     event = "VeryLazy",
     opts = true,
   },
-  -- often used for parsing json5 files from vscode
-  {
-    "Joakker/lua-json5",
-    build = "./install.sh",
-    -- lazy = true,
-    config = function()
-      local json5 = require("json5")
-      require("dap.ext.vscode").json_decode = function(...)
-        p("Using json5 for launch.json")
-        json5.parse(...)
-      end
-    end,
-  },
   {
     "glepnir/lspsaga.nvim",
     opts = {
