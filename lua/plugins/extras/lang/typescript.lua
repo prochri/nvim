af14daa4bd8a87942039cf12a2c0a062cbdfe7dc
Christoph Probst <38609485+prochri@users.noreply.github.com>
2024-08-18 11:09:26 +0200

    use tsserver instead of vtsls

--- lua/plugins/extras/lang/typescript.lua
+++ lua/plugins/extras/lang/typescript.lua
@@ -1,35 +1,36 @@
 return {
-  { import = "lazyvim.plugins.extras.lang.typescript" },
-  {
-    "neovim/nvim-lspconfig",
-    opts = function(_, opts)
-      ---@type lspconfig.settings.vtsls
-      local settings = opts.servers.vtsls.settings
-      settings.vtsls.autoUseWorkspaceTsdk = true
-      -- this seems to be slower than client side filtering
-      settings.vtsls.enableServerSideFuzzyMatch = true
-      settings.vtsls.experimental = settings.vtsls.experimental or {}
-      settings.vtsls.experimental.maxInlayHintLength = 50
-      settings.typescript.tsserver = settings.typescript.tsserver or {}
-      settings.typescript.tsserver.maxTsServerMemory = 8096
-      settings.typescript.watchOptions = {
-        watchFile = "useFsEvents",
-        watchDirectory = "useFsEvents",
-      }
-
-      return opts
-    end,
-  },
-  {
-    "yioneko/nvim-vtsls",
-    lazy = true,
-    opts = {},
-    config = function(_, opts)
-      require("vtsls").config(opts)
-    end,
-  },
+  -- { import = "lazyvim.plugins.extras.lang.typescript" },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   opts = function(_, opts)
+  --     ---@type lspconfig.settings.vtsls
+  --     local settings = opts.servers.vtsls.settings
+  --     settings.vtsls.autoUseWorkspaceTsdk = true
+  --     settings.vtsls.experimental = settings.vtsls.experimental or {}
+  --     settings.vtsls.experimental.maxInlayHintLength = 50
+  --     settings.vtsls.experimental.completion = settings.vtsls.experimental.completion or {}
+  --     -- this seems to be slower than client side filtering
+  --     settings.vtsls.experimental.completion.enableServerSideFuzzyMatch = false
+  --     settings.typescript.tsserver = settings.typescript.tsserver or {}
+  --     settings.typescript.tsserver.maxTsServerMemory = 10000
+  --     -- settings.typescript.watchOptions = {
+  --     --   watchFile = "useFsEvents",
+  --     --   watchDirectory = "useFsEvents",
+  --     -- }
+  --
+  --     return opts
+  --   end,
+  -- },
+  -- {
+  --   "yioneko/nvim-vtsls",
+  --   lazy = true,
+  --   opts = {},
+  --   config = function(_, opts)
+  --     require("vtsls").config(opts)
+  --   end,
+  -- },
   -- using the old lazyvim typescript
-  -- { import = "plugins.extras.lang.typescript_lazyvim_old" },
+  { import = "plugins.extras.lang.typescript_lazyvim_old" },
   {
     "marilari88/twoslash-queries.nvim",
     config = function(_, opts)
