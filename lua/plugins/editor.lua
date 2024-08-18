8d792d5f4c31c98d541fa5012d4550608824e23f
Christoph Probst <38609485+prochri@users.noreply.github.com>
2024-08-18 16:23:07 +0200

    add gitlinker plugin

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -146,6 +146,15 @@ return {
       return opts
     end,
   },
+  {
+    "linrongbin16/gitlinker.nvim",
+    cmd = "GitLink",
+    opts = {},
+    keys = {
+      { "<leader>gy", "<cmd>GitLink<cr>", mode = { "n", "v" }, desc = "Yank git link" },
+      { "<leader>gY", "<cmd>GitLink!<cr>", mode = { "n", "v" }, desc = "Open git link" },
+    },
+  },
   { "anuvyklack/hydra.nvim" },
   { "tpope/vim-rsi" },
   {
